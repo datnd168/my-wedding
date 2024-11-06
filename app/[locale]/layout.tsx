77369bd4ede03b4de2194b76929a254b7e84e170
danionguyen <datnd168@gmail.com>
2024-11-06 11:00:47 +0700

    init

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,50 @@
+import cx from "classnames";
+import { useLocale } from "next-intl";
+import { notFound } from "next/navigation";
+
+import FadeInSection from "@/components/shared/FadeInSection";
+import { inter, sacra, sfPro } from "../fonts";
+
+import { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Đình Đạt - Hải Yến",
+  themeColor: "#FFF",
+  description: "Lễ thành hôn của Đình Đạt và Hải Yến",
+  metadataBase: new URL("https://www.medialinkvn.com/"),
+  openGraph: {
+    type: "website",
+    siteName: "Đình Đạt - Hải Yến",
+    url: "https://www.medialinkvn.com/",
+  },
+  twitter: {
+    site: "@site",
+    title: "Đình Đạt - Hải Yến",
+    description: "Lễ thành hôn của Đình Đạt và Hải Yến",
+  },
+};
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const locale = useLocale();
+
+  if (params.locale !== locale) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale}>
+      <body className={cx(sfPro.variable, inter.variable, sacra.variable)}>
+        <main className="min-h-screen w-full">
+          <FadeInSection>{children}</FadeInSection>
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ configs.ts
@@ -0,0 +1,14 @@
+const configs = {
+  husbandName: "Đình Đạt",
+  wifeName: "Hải Yến",
+  husbandBankNumber: "2206200068999",
+  wifeBankNumber: "02191205301",
+  husbandBankName: "NGUYEN DINH DAT",
+  wifeBankName: "PHUNG THI HAI YEN",
+  address_husband: "Thôn Cánh Chủ, Xã Bình Yên, Huyện Thạch Thất, Hà Nội",
+  address_wife: "Thôn 5, Xã Hạ Bằng, Huyện Thạch Thất, Hà Nội",
+  time_wedding: "2024-11-10T12:00:00",
+  txtTimeWedding: "Chủ nhật ngày 10 tháng 11 năm 2024",
+};
+
+export default configs;
